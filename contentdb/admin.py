2ece47d714f645214242b7fc64b911ef83567d1e
Quentin Bazin <quent42340@gmail.com>
2020-02-26 20:18:03 +0900

    [contentdb] Models created.

--- contentdb/admin.py
+++ contentdb/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Mod, ModVersion, TexturePack, TexturePackVersion
+
+class ModAdmin(admin.ModelAdmin):
+    list_display   = ('name', 'user')
+    list_filter    = ('name', 'user')
+    ordering       = ('name', 'user')
+    search_fields  = ('name', 'user')
+
+class ModVersionAdmin(admin.ModelAdmin):
+    list_display   = ('mod', 'version', 'date', 'doc')
+    list_filter    = ('mod', )
+    ordering       = ('mod', 'version', 'date')
+    search_fields  = ('mod', 'version')
+
+class TexturePackAdmin(admin.ModelAdmin):
+    list_display   = ('name', 'user')
+    list_filter    = ('name', 'user')
+    ordering       = ('name', 'user')
+    search_fields  = ('name', 'user')
+
+class TexturePackVersionAdmin(admin.ModelAdmin):
+    list_display   = ('texture_pack', 'version', 'date', 'doc')
+    list_filter    = ('texture_pack', )
+    ordering       = ('texture_pack', 'version', 'date')
+    search_fields  = ('texture_pack', 'version')
+
+admin.site.register(Mod, ModAdmin)
+admin.site.register(ModVersion, ModVersionAdmin)
+admin.site.register(TexturePack, TexturePackAdmin)
+admin.site.register(TexturePackVersion, TexturePackVersionAdmin)
+
--- contentdb/models.py
+++ contentdb/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Mod(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="mods")
+
+    class Meta:
+        verbose_name = "Mod"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+class ModVersion(models.Model):
+    mod = models.ForeignKey(Mod, on_delete=models.CASCADE, related_name="versions")
+    version = models.CharField(max_length=10, null=False, unique=True)
+    date = models.DateTimeField(default=timezone.now)
+    doc = models.FileField(verbose_name="File", max_length=1000)
+
+    class Meta:
+        verbose_name = "Mod version"
+        ordering = ['date']
+
+    def __str__(self):
+        return self.mod.name + " " + self.version
+
+class TexturePack(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="texture_packs")
+
+    class Meta:
+        verbose_name = "Texture pack"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+class TexturePackVersion(models.Model):
+    texture_pack = models.ForeignKey(TexturePack, on_delete=models.CASCADE, related_name="versions")
+    version = models.CharField(max_length=10, null=False, unique=True)
+    date = models.DateTimeField(default=timezone.now)
+    doc = models.FileField(verbose_name="File", max_length=1000)
+
+    class Meta:
+        verbose_name = "Texture pack version"
+        ordering = ['date']
+
+    def __str__(self):
+        return self.texture_pack.name + " " + self.version
+
