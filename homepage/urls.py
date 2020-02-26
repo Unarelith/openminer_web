8b663009bb7641f047497f71640f24a434462626
Quentin Bazin <quent42340@gmail.com>
2020-02-26 22:17:25 +0900

    Login form added.

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,10 +1,16 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from . import views
 
 app_name = "homepage"
 
+login_view = auth_views.LoginView.as_view(template_name="login_form.html")
+logout_view = auth_views.LogoutView.as_view()
+
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('',       views.home,  name='home'),
+    path('login',  login_view,  name='login'),
+    path('logout', logout_view, name='logout'),
 ]
 
